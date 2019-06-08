9b9585eddc45effd676864e933a2bf7751069507
Maliao Guo <MaliaoTW@gmail.com>
2019-06-08 10:10:47 +0800

    20190608

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,13 +1,23 @@
-from django.urls import path
+from snippets.views import SnippetViewSet, UserViewSet, api_root
+from rest_framework import renderers
 from rest_framework.urlpatterns import format_suffix_patterns
-from snippets import views
+from django.urls import path
 
-# API endpoints
-urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
-    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', views.SnippetHighlight.as_view(), name='snippet-highlight'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail')
-])
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from snippets import views
+from rest_framework.schemas import get_schema_view
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'users', views.UserViewSet)
+
+# The API URLs are now determined automatically by the router.
+
+schema_view = get_schema_view(title='Pastebin API')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('schema/', schema_view),
+]
